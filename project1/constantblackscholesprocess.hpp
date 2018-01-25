494de9a662a01155116107d5829990557bed9d7e
CorentinHR <33058637+CorentinHR@users.noreply.github.com>
2018-01-25 15:17:17 +0100

    Update constantblackscholesprocess.hpp

--- project1/constantblackscholesprocess.hpp
+++ project1/constantblackscholesprocess.hpp
@@ -1,13 +1,44 @@
+#ifndef constant_blackscholes_process_hpp
+#define constant_blackscholes_process_hpp
 
 #include <ql/stochasticprocess.hpp>
+#include <ql/termstructures/yieldtermstructure.hpp>
+#include <ql/termstructures/volatility/equityfx/blackvoltermstructure.hpp>
 
 namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
 
         // your implementation goes here
-        
+    private:
+		Handle<Quote> x0_;
+		Handle<YieldTermStructure> riskFreeRate_;
+		Handle<YieldTermStructure> 	dividendYield_;
+		Handle<BlackVolTermStructure> blackVolatility_;
+		Real constDrift_;
+		Real constDiffusion_;
+		Date exerciceDate_;
+	public:
+
+		constantBlackScholesModel(
+			const Handle <Quote> x0,
+			const Date exerciceDate,
+			const Handle<YieldTermStructure>& riskFreeTS,
+			const Handle<BlackVolTermStructure>& blackVolTS,
+			const Handle<YieldTermStructure>& dividendYieldTS,
+			boost::shared_ptr<discretization>& disc);
+
+		Real drift(Time t, Real x) const;
+
+		Real diffusion(Time t, Real x) const;
+
+
+		Real variance(const StochasticProcess1D&,
+			Time t0, Real x0, Time dt) const;
+
+		Real x0() const;
     };
 
 }
 
+#endif
